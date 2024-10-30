65a82e152590a29970dd60531d077bd3b9b21f46
Caroline Robinson <CRobinsonAtWork@gmail.com>
2024-10-30 11:35:21 -0800

    added required is false and commented out 'disable if: interim_order_period' for both interim and final orders, did not add code or change accordion steps

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.ChangingOrEnforcingYourChildSupportOrder',
-      version='2024.10.29',
+      version='2024.10.30',
       description=('A docassemble extension.'),
       long_description='# docassemble.ChangingOrEnforcingYourChildSupportOrder\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nCaroline Robinson, CRobinson@akcourts.gov\r\n\r\n',
       long_description_content_type='text/markdown',
